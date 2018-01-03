56d278e806dac575dbe2dea100b578001cd5866d
mdodson1995 <m.dodson@hotmail.com>
2018-01-02 20:36:38 -0500

    Built Employee Model

--- WebApplication/WebApplication/Models/Employee.cs
+++ WebApplication/WebApplication/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,19 @@ namespace WebApplication.Models
 {
     public class Employee
     {
+        [Key]
         public int ID { get; set; }
+
+        [Display(Name = "First Name")]
+        public string EmpFirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string EmpLastName { get; set; }
+
+        [Display(Name = "Phone Number")]
+        public string EmpPhone { get; set; }
+
+        [Display(Name = "Email")]
+        public string EmpEmail { get; set; }
     }
 }
